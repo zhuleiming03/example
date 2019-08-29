8fb774a8b0f568726c2f9f6ca2fa6bdc017a4ac1
zhuleiming <zlm1234!!>
2019-08-29 15:02:25 +0800

    初始化项目

---
+++ scheduled/src/main/java/com/example/scheduled/task/SimpleTask.java
@@ -0,0 +1,13 @@
+package com.example.scheduled.task;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleTask {
+
+    @Scheduled(cron = "${jobs.scheduled.simpleTask}")
+    public void simpleTask(){
+        System.out.println("This is a Plan Task Test!");
+    }
+}
