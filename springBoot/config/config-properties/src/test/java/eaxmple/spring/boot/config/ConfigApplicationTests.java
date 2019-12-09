9a80822a0327cd38652815f57ca7c8b434985cff
zhuleiming <zlm1234!!>
2019-12-09 09:37:16 +0800

    add test

---
+++ springBoot/config/config-properties/src/test/java/eaxmple/spring/boot/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package eaxmple.spring.boot.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("This is a config test!");
+    }
+}
