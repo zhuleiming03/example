b53fc1057e6885b6c40ffb79db39e39c749cff2a
zhuleiming <zlm1234!!>
2019-12-20 18:38:45 +0800

    mssql batch insert and update

--- springBoot/mybatis-plus/mybatis-plus-xml-mulidatasource/src/main/java/example/spring/boot/mybatis/plus/xml/mulidatasource/mapper/mssql/UserMapper.java
+++ springBoot/mybatis-plus/mybatis-plus-xml-mulidatasource/src/main/java/example/spring/boot/mybatis/plus/xml/mulidatasource/mapper/mssql/UserMapper.java
@@ -3,6 +3,7 @@ package example.spring.boot.mybatis.plus.xml.mulidatasource.mapper.mssql;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import example.spring.boot.mybatis.plus.xml.mulidatasource.po.mssql.UserPO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,5 +15,9 @@ public interface UserMapper extends BaseMapper<UserPO> {
     void crateTable();
 
     List<UserPO> selectAll();
+
+    void batchUpdate(@Param("list") List<UserPO> list);
+
+    void batchInsert(@Param("list") List<UserPO> list);
 
 }
--- springBoot/mybatis-plus/mybatis-plus-xml-mulidatasource/src/test/java/example/spring/boot/mybatis/plus/xml/mulidatasource/MssqlTest.java
+++ springBoot/mybatis-plus/mybatis-plus-xml-mulidatasource/src/test/java/example/spring/boot/mybatis/plus/xml/mulidatasource/MssqlTest.java
@@ -10,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.List;
 
 @SpringBootTest
 class MssqlTest {
@@ -51,11 +53,61 @@ class MssqlTest {
             System.out.println(">>删除成功");
         }
         assert result == 1;
+
+        //批量插入更新查询测试
+        batchTest();
     }
 
-
     @Autowired
-    UserMapper mapper;
+    private UserMapper mapper;
+
+    private void batchTest() {
+
+        //初始化数据
+        List<UserPO> userPOList = new LinkedList<>();
+        UserPO po1 = initUserPO();
+        userPOList.add(po1);
+        UserPO po2 = initUserPO();
+        po2.setName("宇智波佐助");
+        po2.setBirthday(LocalDate.of(2000, 7, 23));
+        po2.setCheapRate(0.8d);
+        po2.setBalance(new BigDecimal(4324432.42f));
+        po2.setCardNo(100200201911280019L);
+        userPOList.add(po2);
+        UserPO po3 = initUserPO();
+        po3.setName("春野樱");
+        po3.setBirthday(LocalDate.of(2000, 3, 28));
+        po3.setCheapRate(0.75d);
+        po3.setBalance(new BigDecimal(105.31f));
+        po3.setSex(UserSexEnum.FAMALE);
+        po3.setCardNo(100200201911280020L);
+        userPOList.add(po3);
+
+        //批量插入数据
+        mapper.batchInsert(userPOList);
+
+        System.out.println(">>批量插入数据:");
+        userPOList.forEach((u) -> {
+            System.out.println(">>" + u);
+        });
+
+
+        //批量查询数据
+        List<UserPO> poList = mapper.selectAll();
+
+        //批量更新数据
+        poList.forEach((u) -> {
+            u.setCheapRate(u.getCheapRate() * 0.8f);
+            u.setBalance(u.getBalance().subtract(new BigDecimal(99)));
+        });
+        mapper.batchUpdate(poList);
+
+        System.out.println(">>批量更新数据:");
+        poList.forEach((u) -> {
+            System.out.println(">>" + u);
+        });
+
+    }
 
     private UserPO initUserPO() {
         //新增记录
@@ -68,7 +120,7 @@ class MssqlTest {
         po.setAge(age);
         po.setCheapRate(0.95d);
         po.setBalance(balance);
-        po.setBirthday(LocalDate.of(1995, 3, 5));
+        po.setBirthday(LocalDate.of(2000, 10, 10));
         po.setCreateTime(new Timestamp(System.currentTimeMillis()));
         po.setVaild(true);
         return po;
