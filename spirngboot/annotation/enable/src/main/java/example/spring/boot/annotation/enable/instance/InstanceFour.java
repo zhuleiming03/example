9e8e5b258559e3f511ef1b6a267d28b9b6622ddf
zhuleiming <zhuleiming03@gmail.com>
2020-04-10 10:13:58 +0800

    remove

--- spirngboot/annotation/enable/src/main/java/example/spring/boot/annotation/enable/instance/InstanceFour.java
+++
@@ -1,9 +0,0 @@
-package example.spring.boot.annotation.enable.instance;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@ComponentScan(basePackages = "example.spring.boot.annotation.enable.service")
-@Configuration
-public class InstanceFour {
-}
--- spirngboot/annotation/enable/src/main/java/example/spring/boot/annotation/enable/instance/InstanceOne.java
+++
@@ -1,9 +0,0 @@
-package example.spring.boot.annotation.enable.instance;
-
-import example.spring.boot.annotation.enable.annotation.EnableHelloWorld;
-import org.springframework.context.annotation.Configuration;
-
-@EnableHelloWorld
-@Configuration
-public class InstanceOne {
-}
